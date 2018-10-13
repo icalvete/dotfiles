5f346bf9a636afc766e517d4e9bda88dc9aea21b
Israel Calvete <icalvete@gmail.com>
2018-10-13 18:16:57 +0200

    Fix tComment issue

--- install.sh
+++ install.sh
@@ -47,6 +47,8 @@ echo
 echo "Write q! after Done!."
 echo 
 sleep 2
+#mkdir -p ~/dotfiles/vim/bundle/vim-snippets/snippets/
+#mkdir -p ~/dotfiles/vim/after/plugin/
 vim  -u ~/dotfiles/bundles.vim +PluginInstall +q!
 echo "Instaled."
 
@@ -54,4 +56,5 @@ echo "Instaling snippets and tools..."
 cp -a ~/dotfiles/files/puppet.snippets ~/dotfiles/vim/bundle/vim-snippets/snippets/puppet.snippets
 cp -a ~/dotfiles/files/sh.snippets ~/dotfiles/vim/bundle/vim-snippets/snippets/sh.snippets
 cp -a ~/dotfiles/files/tabular.vim ~/dotfiles/vim/after/plugin/tabular.vim
+cp -a ~/dotfiles/files/tComment.vim ~/dotfiles/vim/after/plugin/tComment.vim
 echo "Instaled."
